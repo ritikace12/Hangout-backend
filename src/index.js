97af9d60ab9b1231600665ea7b7d704cd59d5214
Ritik Meena <ritikm361@gmail.com>
2025-04-03 21:33:42 +0530

    Your commit message

--- src/index.js
+++ src/index.js
@@ -16,7 +16,7 @@ const server = createServer(app)
 // Socket.io setup
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:5173",
+    origin: ["http://localhost:5173", "https://hangout-12.netlify.app"],
     credentials: true,
   },
   pingTimeout: 60000,
@@ -26,7 +26,7 @@ const io = new Server(server, {
 app.use(express.json({ limit: "50mb" }))
 app.use(cookieParser())
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: ["http://localhost:5173", "https://hangout-12.netlify.app"],
   credentials: true,
 }))
 
